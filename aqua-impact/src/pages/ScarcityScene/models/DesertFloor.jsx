bf011ce68f8e96d4026d377305e7d456f5356368
MiguelTami <miguel.tami@correounivalle.edu.co>
2024-11-19 13:11:02 -0500

    Physics and Camel added

--- aqua-impact/src/pages/ScarcityScene/models/DesertFloor.jsx
+++ aqua-impact/src/pages/ScarcityScene/models/DesertFloor.jsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { useTexture } from "@react-three/drei";
 import { RepeatWrapping } from "three";
+import { RigidBody } from "@react-three/rapier";
 
 const DesertFloor = () => {
   const PATH = useMemo(() => "textures/desert/mud_cracked_dry_03_", []);
@@ -9,29 +10,29 @@ const DesertFloor = () => {
     map: PATH + "diff_2k.jpg",
     displacementMap: PATH + "disp_2k.png",
     normalMap: PATH + "nor_gl_2k.jpg",
-    armMap: PATH + "arm_2k.jpg"
+    armMap: PATH + "arm_2k.jpg",
   });
 
+  Object.keys(floorTexture).map((key) => {
+    floorTexture[key].wrapS = floorTexture[key].wrapT = RepeatWrapping;
+    floorTexture[key].repeat.set(8, 8);
+  });
 
-   Object.keys(floorTexture).map((key)=>{
-     floorTexture[key].wrapS = floorTexture[key].wrapT = RepeatWrapping
-     floorTexture[key].repeat.set(8,8);
-   })
-   
   return (
-    
-    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeGeometry args={[400, 400, 400, 400]} />
-      <meshStandardMaterial
-        ambientOcclusionMap={floorTexture.armMap}
-        map={floorTexture.map}
-        displacementMap={floorTexture.displacementMap}
-        roughnessMap={floorTexture.armMap}
-        normalMap={floorTexture.normalMap}
-        metalnessMap={floorTexture.armMap}
-      />
-    </mesh>
+    <RigidBody>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+        <planeGeometry args={[400, 400, 400, 400]} />
+        <meshStandardMaterial
+          ambientOcclusionMap={floorTexture.armMap}
+          map={floorTexture.map}
+          displacementMap={floorTexture.displacementMap}
+          roughnessMap={floorTexture.armMap}
+          normalMap={floorTexture.normalMap}
+          metalnessMap={floorTexture.armMap}
+        />
+      </mesh>
+    </RigidBody>
   );
-}
+};
 
 export default DesertFloor;
